ad53ef8c2d9e8088468471ad47d0705d3acf7ed0
Michael Halili <97466547+haleelee@users.noreply.github.com>
2023-10-19 16:57:13 -0500

    Update placeSleeveByComment.py
    Added for loop to add Aerator Sleeves to the end of the list. Based on Comment value "AERATOR"

--- placeSleeveByComment.py
+++ placeSleeveByComment.py
@@ -27,10 +27,12 @@ six_in_sleeve = UnwrapElement(IN[8])
 eight_in_sleeve = UnwrapElement(IN[9])
 ten_in_sleeve = UnwrapElement(IN[10])
 twelve_in_sleeve = UnwrapElement(IN[11])
+elevation = UnwrapElement(IN[12])
 
 out = []
 
 # Place your code below this line
+#for e in elements:
 for e in elements:
 	p = []
 	sleeve = 0;
@@ -47,6 +49,11 @@ for e in elements:
 		sleeve = six_in_sleeve
 	elif comment == "UP TO TB":
 		sleeve = two_in_tb_sleeve
+	#elif comment == "AERATOR":
+		#sleeve = four_in_aerator_sleeve
+		#elevation = elevation - 4
+	elif comment == "UP TO MC":
+		sleeve = two_in_sleeve
 	else:
 		if size <= 0.175:
 			sleeve = two_in_sleeve
@@ -60,23 +67,7 @@ for e in elements:
 			sleeve = ten_in_sleeve
 		elif size <= 2:
 			sleeve = twelve_in_sleeve
-			"""
-	if not bool(comment):
-		if size <= 0.175:
-			sleeve = two_in_sleeve
-		elif size <= 0.26:
-			sleeve = three_in_sleeve
-		elif size <= 0.35:
-			sleeve = four_in_sleeve
-		elif size <= 0.51:
-			sleeve = six_in_sleeve
-		elif size <= 0.668:
-			sleeve = ten_in_sleeve
-		elif size <= 2:
-			sleeve = twelve_in_sleeve
-		#else
-			#TaskDialog.Show("Example Title", "Example Text")
-	"""
+
 	p.append(origin_x)
 	p.append(origin_y)
 	p.append(origin_z)
@@ -85,9 +76,33 @@ for e in elements:
 	p.append(comment)
 	p.append(sleeve)
 	p.append(e.Id)
+	p.append(elevation)
 	out.append(p)
 
-
+#The code below adds the aerator adapter sleeve for any vertical pipes with Comment value "AERATOR"
+for e in elements:
+	comment = e.LookupParameter("Comments").AsString()
+	if comment == "AERATOR":
+		q = []
+		sleeve = 0;
+		origin = e.Origin
+		origin_x = origin.X
+		origin_y = origin.Y
+		origin_z = origin.Z
+		size = e.LookupParameter("Inside Diameter").AsDouble()
+		sleeve = four_in_aerator_sleeve
+		elevation = elevation - 4
+		
+		q.append(origin_x)
+		q.append(origin_y)
+		q.append(origin_z)
+		q.append(level)
+		q.append(size)
+		q.append(comment)
+		q.append(sleeve)
+		q.append(e.Id)
+		q.append(elevation)
+		out.append(q)
 
 # Assign your output to the OUT variable.
 OUT = out
